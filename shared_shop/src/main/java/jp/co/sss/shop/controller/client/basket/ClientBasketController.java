abbe6cd8ba4523923b0691e0875fed12bba49e29
r-kido <r-kido@CD26306L.arksystems.co.jp>
2026-06-08 17:26:04 +0900

    feat メソッドの追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -1,5 +1,28 @@
 package jp.co.sss.shop.controller.client.basket;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class ClientBasketController {
+
+	@RequestMapping(path = "/client/basket/add", method = RequestMethod.POST)
+	public String addBasket() {
+
+		return "redirect :/client/basket/list";
+	}
+
+	@RequestMapping(path = "/client/basket/delete", method = RequestMethod.POST)
+	public String deleteBasket() {
+
+		return "redirect:/client/basket/list ";
+	}
+
+	@RequestMapping(path = "/basket/allDelete", method = RequestMethod.POST)
+	public String alldeleteBasket() {
+
+		return "redirect: /client/basket/list";
+	}
 
 }
