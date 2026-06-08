0541cac679deca5157ba8723c1e4bb63cd9f2869
takuriha <takuriha@C010423243.zipangu.cac.co.jp>
2026-06-08 17:01:24 +0900

    feat パスとビューの追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
@@ -1,5 +1,15 @@
 package jp.co.sss.shop.controller.client.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ClientUserUpdateController {
+
+	@RequestMapping(path = "/client/user/update/input")
+	public String updateUserinput() {
+
+		return "user/update_input";
+	}
 
 }
