0aa6d6741a4bd16ed8c1698bce1be9b0de1b80c0
takuriha <takuriha@C010423243.zipangu.cac.co.jp>
2026-06-09 10:14:36 +0900

    feat パス追加。未完成故コメントアウト済

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
@@ -1,15 +1,24 @@
 package jp.co.sss.shop.controller.client.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ClientUserUpdateController {
 
-	@RequestMapping(path = "/client/user/update/input")
-	public String updateUserinput() {
+	@RequestMapping(path = "/client/user/update/input", method = RequestMethod.POST)
+	public String updateUserinput(Model model, HttpSession session) {
 
 		return "user/update_input";
 	}
 
+	//	@RequestMapping(path ="/client/user/update/check",method = RequestMethod.POST)
+	//	public String updateUsercheck()
+	//	
+	//	
+	//
 }
