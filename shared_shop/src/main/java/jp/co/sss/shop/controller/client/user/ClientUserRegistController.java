8c398d87ab34158d54517bc8ab4c7569bda4e153
kawachif <kawachif@C010423162.zipangu.cac.co.jp>
2026-06-09 12:27:22 +0900

    feat:会員登録機能の追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -1,30 +1,62 @@
 package jp.co.sss.shop.controller.client.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.UserForm;
+import jp.co.sss.shop.repository.UserRepository;
 
 @Controller
 public class ClientUserRegistController {
 
-	@RequestMapping(path = "/client/user/regist/check", method = RequestMethod.POST)
-	public String inputUser(@ModelAttribute UserForm form) {
-		return "regist_input";
+	@Autowired
+	HttpSession session;
+	@Autowired
+	UserRepository repository;
+
+	@RequestMapping(path = { "/client/user/regist/input/init", "/client/user/regist/input" })
+	public String registInput(Model model) {
+		UserForm form = (UserForm) session.getAttribute("userForm");
+		if (form == null) {
+			form = new UserForm();
+			session.setAttribute("userForm", form);
+		}
+		model.addAttribute("userForm", form);
+		return "client/user/regist_input";
+	}
+
+	@RequestMapping(path = "/client/user/regist/check", method = { RequestMethod.POST, RequestMethod.GET })
+	public String registCheck(@Valid @ModelAttribute UserForm form, BindingResult result, Model model) {
+		session.setAttribute("userForm", form);
+		if (result.hasErrors()) {
+			model.addAttribute("userForm", form);
+			return "client/user/regist_input";
+		}
+
+		model.addAttribute("userForm", form);
+		return "client/user/regist_check";
 	}
 
 	@RequestMapping(path = "/client/user/regist/complete", method = RequestMethod.POST)
-	public String checkUser(@Valid @ModelAttribute UserForm form, Model model) {
-		return "regist_check";
-	}
+	public String registComplete() {
+		UserForm form = (UserForm) session.getAttribute("userForm");
 
-	@RequestMapping("/shared_shop")
-	public String completeUser() {
-		return "regist_complete";
-	}
+		if (form != null) {
+			User user = new User();
+			user.setName(form.getName());
+			repository.save(user);
+			session.removeAttribute("userForm");
+			session.setAttribute("user", user);
+		}
 
+		return "client/user/regist_complete";
+	}
 }
