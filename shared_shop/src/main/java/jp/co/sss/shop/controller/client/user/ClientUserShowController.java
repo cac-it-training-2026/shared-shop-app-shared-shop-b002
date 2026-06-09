d016f9b098b97d0a6f09f6626069d4cc4f31308b
takuriha <takuriha@C010423243.zipangu.cac.co.jp>
2026-06-09 14:21:19 +0900

    feat:会員詳細情報表示（仮）

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserShowController.java
@@ -1,5 +1,36 @@
 package jp.co.sss.shop.controller.client.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.repository.UserRepository;
+
+@Controller
 public class ClientUserShowController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping(path = "/client/user/detail", method = RequestMethod.GET)
+	public String showDetailUser(HttpSession session, Model model) {
+
+		UserBean loginUser = (UserBean) session.getAttribute("user");
+
+		User user = userRepository.findByEmail(loginUser.getEmail());
+
+		UserBean userBean = new UserBean();
+
+		//		BeanUtils.copyProperties(user, userBean);
+
+		model.addAttribute("userBean", userBean);
+
+		return "client/user/detail";
+	}
 
 }
