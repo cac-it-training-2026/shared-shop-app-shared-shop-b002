a4559b4e93a8c7a39f74481e3d816df58a68ccb1
takuriha <takuriha@C010423243.zipangu.cac.co.jp>
2026-06-10 10:50:04 +0900

    fix:戻るボタンの送信方式の変更。update_input.html

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserUpdateController.java
@@ -6,14 +6,5 @@ import org.springframework.stereotype.Controller;
 public class ClientUserUpdateController {
 
 	//	@RequestMapping(path = "/client/user/update/input", method = RequestMethod.POST)
-	//	public String updateUserinput(Model model, HttpSession session) {
-	//
-	//		return "user/update_input";
-	//	}
 
-	//	@RequestMapping(path ="/client/user/update/check",method = RequestMethod.POST)
-	//	public String updateUsercheck()
-	//	
-	//	
-	//
 }
